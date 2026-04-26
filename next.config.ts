4917d282d4ec5c3e37529552b10ea3f8df7493d9
Atswik <satwiktungala@gmail.com>
2026-04-25 20:09:58 -0400

    Add serverExternalPackages for playwright-core and @sparticuz/chromium

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  serverExternalPackages: ['@sparticuz/chromium', 'playwright-core'],
 };
 
 export default nextConfig;
